3be9580df42efd75d33dfbfbba43cd86eb3f00c7
shirishmakwana <shirishm.makwana@gmail.com>
2022-04-02 18:42:34 +0530

    regular interval notification added

--- background.js
+++ background.js
@@ -2,6 +2,10 @@
 
 chrome.runtime.onInstalled.addListener(() => {
     console.log('%c Yay, Extension is installed!', 'background: green; color:white');
+
+    // create alarm after extension is installed / upgraded
+    chrome.alarms.create("startRequest", { periodInMinutes: 4 });
+    startRequest();
 });
 
 chrome.runtime.onMessage.addListener(data => {
@@ -18,3 +22,22 @@ const notify = message => {
         iconUrl: './images/icon128.png',
     });
 };
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    startRequest();
+});
+
+async function startRequest() {
+    const response = await fetch("https://api.quotable.io/random");
+    const newData = await response.json();
+    const data = `${newData.content} —${newData.author}`;
+    console.log(data);
+
+    var options = {
+        title: "Random Quotes",
+        message: data,
+        iconUrl: "/images/icon128.png",
+        type: "basic",
+    };
+    chrome.notifications.create("", options);
+}
